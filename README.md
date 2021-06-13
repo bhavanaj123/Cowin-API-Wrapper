59f939dcebf524f2f4b77d0c739143ea5b179cf8
MaradiSu <p.suresh@campusuvce.in>
2021-06-13 18:52:39 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cowin Node API
---
+++ index.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch');
+
+fetch('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=583212&date=15-05-2021',{
+    
+  method: 'GET',
+  headers: {
+    accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+'accept-encoding': 'gzip, deflate, br',
+'accept-language': 'en-US,en;q=0.9,kn;q=0.8',
+'cache-control': 'max-age=0',
+'if-none-match': 'W/"623-dhjKNwDguPe93PO7B8mabFkLR3A',
+'sec-ch-ua': 'Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+'sec-ch-ua-mobile': '?1',
+'sec-fetch-dest': 'document',
+'sec-fetch-mode': 'navigate',
+'sec-fetch-site': 'none',
+'sec-fetch-user': '?1',
+'upgrade-insecure-requests': '1',
+'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Mobile Safari/537.36',
+    
+  }
+})
+    .then(res => res.json())
+    .then(json => {
+        console.log("First user in the array:");
+        console.log(json.centers);
+        
+})
