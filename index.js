b548f89e74b698736b23c245497e0ba8dd8b8a5f
MaradiSu <p.suresh@campusuvce.in>
2021-06-25 14:24:26 +0530

    chnaged structure

--- index.js
+++ index.js
@@ -18,14 +18,37 @@ const fetch = require('node-fetch');
 myObj = new Object()
 
  app.get('/states', function (req, res) {
-  
-  fetch('https://cdn-api.co-vin.in/api/v2/admin/location/states')
-          .then(res => res.json())
-          .then(json => {
-            res.send( (json.states));
-              
-      })
-                  
+ // let pin = req.query.pin;
+ // let date = req.query.date;
+ // console.log('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode='+pin+'&date='+date);
+      fetch('https://cdn-api.co-vin.in/api/v2/admin/location/states',{
+      
+    method: 'GET',
+    headers: {
+      accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  'accept-encoding': 'gzip, deflate, br',
+  'accept-language': 'en-US,en;q=0.9,kn;q=0.8',
+  'cache-control': 'max-age=0',
+  'if-none-match': 'W/"623-dhjKNwDguPe93PO7B8mabFkLR3A',
+  'sec-ch-ua': 'Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+  'sec-ch-ua-mobile': '?1',
+  'sec-fetch-dest': 'document',
+  'sec-fetch-mode': 'navigate',
+  'sec-fetch-site': 'none',
+  'sec-fetch-user': '?1',
+  'upgrade-insecure-requests': '1',
+  'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Mobile Safari/537.36',
+      
+    }
+  })
+      .then(res => res.json())
+      .then(json => {
+          //console.log("First user in the array:");
+          //console.log(json.centers);
+          res.end( JSON.stringify(json.states));
+          
+  })
+               
                   
           })      
  app.get('/dsts', function (req, res) {
@@ -90,7 +113,7 @@ app.get('/bydist', function (req, res) {
               .then(json => {
                   //console.log("First user in the array:");
                   //console.log(json.centers);
-                  res.end( JSON.stringify(json.sessions));
+                  res.end( (json.sessions));
                   
           })
                       
